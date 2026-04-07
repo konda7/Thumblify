53752f48cb872b986871e9fbb05af48dad7a556b
Your Name <you@example.com>
2026-04-07 13:40:27 +0530

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
 import LenisScroll from "./components/LenisScroll";
+import Generate from "./pages/Generate";
+import MyGenerations from "./pages/MyGenerations";
+import YtPreview from "./pages/YtPreview";
+import Login from "./pages/Login";
 
 export default function App() {
     return (
@@ -12,6 +16,11 @@ export default function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/generate" element={<Generate />} />
+                <Route path="/generate/:id" element={<Generate />} />
+                <Route path="/my-generation" element={<MyGenerations />} />
+                <Route path="/preview" element={<YtPreview />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
             <Footer />
         </>
